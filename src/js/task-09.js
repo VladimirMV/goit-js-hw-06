7a47df70e3b752b7985063c03dd055e823688763
Vladimir Minakov <Vladimirmvk@gmail.com>
2023-01-23 13:46:52 +0200

    9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const buttonChangeColor = document.querySelector('.change-color');
+const output = document.querySelector('.color');
+const body = document.querySelector('body');
+
+buttonChangeColor.addEventListener('click', changeColor);
+
+function changeColor(event) {
+  let colorRandom = getRandomHexColor();
+  body.style.backgroundColor = colorRandom;
+  output.textContent = colorRandom;
+  
+   
+}
