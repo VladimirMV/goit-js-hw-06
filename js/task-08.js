57c27c920abbd8846d0a8b139d3f7e1f78b96f28
Vladimir Minakov <Vladimirmvk@gmail.com>
2023-01-23 12:17:23 +0200

    9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
 
+
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+  
+  const {
+        elements: { email, password },
+    } = event.currentTarget
+
+    if (email.value === '' || password.value === '') {
+        return alert('All fields are required to be completed!')
+    }
+  const formData = new FormData(event.currentTarget);
+    let userData = [];
+    formData.forEach((value, name) => {
+        userData.push({ name, value });
+    }) 
+    
+    console.log(userData)
+     
+    event.currentTarget.reset()
+}
